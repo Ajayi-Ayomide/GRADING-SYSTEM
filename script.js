358d1adf6cb8d4553f24fd0ff20c1674e042c1df
Ajayi Ayomide <ajayiayomide34@gmail.com>
2024-03-12 19:16:20 +0100

    first commit

--- script.js
+++ script.js
@@ -17,40 +17,44 @@ var songToPlay5 = new Audio (song5)
 var song6 = "Audio/VID-20231205-WA0000 (2).mp3"
 var songToPlay6 = new Audio (song6)
 
+var song7 = "Audio/error-170796.mp3"
+var songToPlay7 = new Audio (song7)
+
 function score(){
     if (num.value != "") {
         if (num.value >= 0 && num.value < 40) {
-            display.value = "F 😭😭😭"
-            display.style.color ="red"
+            broadcast.innerHTML = `F stands for Flunked. Your grade is F, and let's be honest, you might as well have not even shown up.😭😭😭`
+            broadcast.style.color = "red"
             songToPlay.play()
         }
         else if (num.value >= 40 && num.value < 45) {
-            display.value = "E"
-            display.style.color ="brown"
+            broadcast.innerHTML = "E for Effort? Not quite. Your grade is E, and it's a testament to how much you've missed the mark. 🙊🙊🙊"
+            broadcast.style.color ="black"
             songToPlay2.play(song2)
     
-        }  else if (num.value >= 45 && num.value < 49) {
-            display.value = "D"
-            display.style.color ="purple"
+        }  else if (num.value >= 45 && num.value < 50) {
+            broadcast.innerHTML = "D is for Dismal. Your grade is D, and it's a clear sign that you need to seriously reevaluate your approach. 🥸🥸🥸"
+            broadcast.style.color ="black"
             songToPlay3.play(song3)
     
-        }  else if (num.value >= 49 && num.value < 55) {
-            display.value = "C"
-            display.style.color ="blue"
+        }  else if (num.value >= 50 && num.value < 60) {
+            broadcast.innerHTML = "C is for Can't Quite Cut It. Your grade is C, and it's just not good enough. Time to buckle down and aim higher. 🐸🐸🐸"
+            broadcast.style.color ="black"
             songToPlay4.play(song4)
     
-        }  else if (num.value >= 55 && num.value < 70) {
-            display.value = "B"
-            display.style.color ="yellow"
+        }  else if (num.value >= 60 && num.value < 75) {
+            broadcast.innerHTML = "Barely scraping by with a B? Sorry, but that's just not going to cut it. Your grade is B, but it's a far cry from success. 🐭🐭🐭"
+            broadcast.style.color ="black"
             songToPlay5.play(song5)
     
-        }  else if (num.value >= 70 && num.value <= 100) {
-            display.value = "A"
-            display.style.color ="Green"
+        }  else if (num.value >= 75 && num.value <= 100) {
+            broadcast.innerHTML = "An A for Average? Not quite. Your grade is A, and while it's good, it's certainly not exceptional. Time to raise the Bar. 👿🥰👏"
+            broadcast.style.color ="Green"
             songToPlay6.play(song6)
         }
         else  {
-           display.value ="You've exceeded the range"
+            songToPlay7.play(song7)
+           broadcast.innerHTML ="You've exceeded the range. 🤺🪓"
         }
     }
 }
